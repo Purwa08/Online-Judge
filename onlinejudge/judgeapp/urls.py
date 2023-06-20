df1d9ee2985ffebf622ab363f007f88c0df144b5
purwa <purwatitarmare08@gmail.com>
2023-06-20 14:39:10 +0530

    done

--- onlinejudge/judgeapp/urls.py
+++ onlinejudge/judgeapp/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
     path('problems/<int:problem_id>/', views.problem_detail, name='problem_detail'),
     path('success/', views.success, name='success'),
     path('leaderboard/', views.leaderboard, name='leaderboard'),
+    path('problems/<int:problem_id>/submit/', views.submit_code, name='submit_code')
+
     #path('register/', views.register, name='register'),
     #path('problems/', views.problem_list, name='problem_list'),
     # Add more URL patterns as needed
