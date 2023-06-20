323e1101135680ccdf861a1bf2d49ce09584adc0
purwa <purwatitarmare08@gmail.com>
2023-06-20 12:36:55 +0530

    3rd

--- onlinejudge/onlinejudge/settings.py
+++ onlinejudge/onlinejudge/settings.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
-"""
+""" 
 
 from pathlib import Path
 
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'judgeapp',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
